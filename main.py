8e07cc85472438ffe8a05f1d4b056e793fb94125
Danny Kellett <development@dannykellett.com>
2024-11-01 21:59:06 +0000

    add requirements.txt

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ load_dotenv()
 
 # Kafka settings
 KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
-KAFKA_TOPIC_CONSUME = "collected_artefacts_dev"
-KAFKA_TOPIC_PRODUCE = "collected_artefacts_dev"
+KAFKA_TOPIC_CONSUME = "collected_artefacts_rss_pre_scrape"
+KAFKA_TOPIC_PRODUCE = "collected_artefacts_rss_post_scrape"
 
 # Database connection details
 POSTGRES_USER = os.getenv("POSTGRES_USER")
